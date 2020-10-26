11c6eb6a0f51b92bf51d7986edba4ce250abff5a
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2020-10-26 11:12:58 -0500

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const apollo = require('apollo-server')
+const mongoose = require('mongoose')
+
+const typeDefs = require('./graphql/typedefs')
+const resolvers = require('./graphql/resolvers/index')
+const { DB_CONNECTION } = process.env
+
+
+const server = new apollo.ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+mongoose.connect(DB_CONNECTION, { useNewUrlParser: true })
+  .then(()=>{
+    console.log('mongodb connected')
+    return server.listen({ port: 5000})
+  })
+  .then((res)=>{
+    console.log(`server running at ${res.url}`)
+  })
